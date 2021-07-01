8b2d4ad788a7650eba6d1af38205cb0e7cb3f397
cypresschess <cypresschess@example.com>
2021-07-01 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"HOUSTON","2021-06-27","4","<a href=http://www.uschess.org/msa/XtblMain.php?202106273132 target=' blank'>INAUG TOURNEY</a>",
 "HOUSTON","2021-06-26","74","<a href=http://www.uschess.org/msa/XtblMain.php?202106261592 target=' blank'>SPINDLETAP SCHOLASTIC & COLLEGIATE</a>",
 "SPRING","2021-06-25","12","<a href=http://www.uschess.org/msa/XtblMain.php?202106251102 target=' blank'>THE FRIDAY KNIGHTS</a>",
 "HOUSTON","2021-06-25","6","<a href=http://www.uschess.org/msa/XtblMain.php?202106251032 target=' blank'>FRIDAYNIGHTJUNE25</a>",
@@ -198,5 +199,4 @@ jtable([
 "TOMBALL","2020-02-21","14","<a href=http://www.uschess.org/msa/XtblMain.php?202002213172 target=' blank'>THE FRIDAY KNIGHTS ADVANCED</a>",
 "HOUSTON","2020-02-21","12","<a href=http://www.uschess.org/msa/XtblMain.php?202002213092 target=' blank'>FRIDAY KNIGHT QUADS 02.21.2020</a>",
 "HOUSTON","2020-02-21","20","<a href=http://www.uschess.org/msa/XtblMain.php?202002213062 target=' blank'>CHESSPANDA02212020FRIDAY</a>",
-"CYPRESS","2020-02-21","12","<a href=http://www.uschess.org/msa/XtblMain.php?202002213042 target=' blank'>KNIGHTS OF CYPRESS</a>",
 ]);
