fe6a2058ad7352a65dc2b70f948baaff69efe92b
cypresschess <cypresschess@example.com>
2021-10-09 01:30:08 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"SUGAR LAND","2021-10-08","4","<a href=http://www.uschess.org/msa/XtblMain.php?202110089262 target=' blank'>FBCC EARLY-OCTOBER CLASSIC 2021</a>",
 "HOUSTON","2021-10-03","6","<a href=http://www.uschess.org/msa/XtblMain.php?202110038642 target=' blank'>SUNDAY SWISS OCT 3RD</a>",
 "SPRING","2021-10-02","8","<a href=http://www.uschess.org/msa/XtblMain.php?202110027682 target=' blank'>DER SPRINGERS</a>",
 "SPRING","2021-10-01","21","<a href=http://www.uschess.org/msa/XtblMain.php?202110017152 target=' blank'>THE FRIDAY KNIGHTS</a>",
@@ -198,5 +199,4 @@ jtable([
 "SPRING","2020-09-04","24","<a href=http://www.uschess.org/msa/XtblMain.php?202009045472 target=' blank'>THE FRIDAY KNIGHTS</a>",
 "KATY","2020-08-29","19","<a href=http://www.uschess.org/msa/XtblMain.php?202008296062 target=' blank'>SR CHESSKNIGHTS 08292020</a>",
 "SPRING","2020-08-28","14","<a href=http://www.uschess.org/msa/XtblMain.php?202008284062 target=' blank'>THE FRIDAY KNIGHTS</a>",
-"SPRING","2020-08-27","4","<a href=http://www.uschess.org/msa/XtblMain.php?202008274092 target=' blank'>CBS DOUBLE ROUND ROBIN</a>",
 ]);
