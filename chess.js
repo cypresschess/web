183124a610faf1b35f9234c0087fcd6383808c62
cypresschess <cypresschess@example.com>
2021-11-09 02:30:37 -0600

    Okay

--- chess.js
+++ chess.js
@@ -2,11 +2,35 @@ news_feed (
 {
    "items" : [
       {
+         "description" : "Exchanging the right pieces is a very important topic, for which you should develop a fine intuition. How should Black proceed in the diagram position?",
+         "iconLink" : "https://cypresschess.github.io/web/chessbase2.png",
+         "iconName" : "ChessBase",
+         "link" : "https://en.chessbase.com/post/the-right-exchange",
+         "pubDate" : "Tue, 09 Nov 2021 09:00:00 +0100",
+         "title" : "The right exchange"
+      },
+      {
+         "description" : "<b>Damod,Sajjad (1358) - Morshedloo,Amin [C45]</b><br />Asian Club Hybrid Chess Championship Tornelo (5.3), 07.11.2021 <script>new PgnViewer( { boardName: \"game1288\", movesFormat: \"default\", pgnString:'1.e4 e5 2.Nf3 Nc6 3.d4 exd4 4.Nxd4 Nxd4 5.Qxd4 Nf6 6.e5 Qe7 7.Be3 c5 8.exf6 cxd4 9.fxe7 dxe3 10.exf8Q+ Rxf8 11.fxe3 d6 12.Bb5+ Bd7 13.Nc3 Bxb5 14.Nxb5 Kd7 15.O-O-O a6 16.Nxd6 Kc7 17.Rhf1 f6 18.Rd3 Rad8 19.Rfd1 b5 20.Nxb5+ axb5 21.Rxd8 Rxd8 22.Rxd8 Kxd8 23.Kd2 Kc7 24.Kc3 Kb6 25.Kb4 g5 26.g4 h6 27.e4 Ka6 28.Kc5 Ka5 29.Kd6 Kb4 30.Ke6 Kc4 31.Kxf6 Kd4 32.e5 Kd5 33.e6 Kd6 34.e7 Kd7 35.Kf7 Kd6 36.e8Q Kc5 37.Qf8+ Kb6 38.Qxh6+ Ka5 39.Qxg5 Kb4 40.h4 Kc4 41.h5 Kd4 42.Qxb5 Ke4 43.g5 Kf4 44.g6 Kg4 45.h6 Kh",
+         "iconLink" : "https://cypresschess.github.io/web/blogger2.png",
+         "iconName" : "Chess Express",
+         "link" : "http://chessexpress.blogspot.com/2021/11/once-knight-always-knight-five-time.html",
+         "pubDate" : "Tue, 09 Nov 2021 07:04:00 +0000",
+         "title" : "Once a knight, always a knight. Five time a night ...."
+      },
+      {
+         "description" : "Kolkata based International Master Mitrabha Guha becomes the 72nd Grandmaster of the country after completing the requirement by securing his third and final GM norm",
+         "iconLink" : "https://cypresschess.github.io/web/aicf.png",
+         "iconName" : "All India Chess<br>Federation",
+         "link" : "https://aicf.in/congratulations-mitrabha-guha-72nd-grandmaster-of-india/",
+         "pubDate" : "Tue, 09 Nov 2021 06:48:39 +0000",
+         "title" : "Congratulations Mitrabha Guha : 72nd Grandmaster of India"
+      },
+      {
          "description" : "In a thrilling opening match of the 2021 Speed Chess Championship Main Event, GM Anish Giri defeated GM Jan-Krzysztof Duda with a final score of 14-12. The next match, between GMs Shakhriyar Mamedyarov and Ding Liren, starts on Tuesday, November 9...",
          "iconLink" : "https://cypresschess.github.io/web/chesscom.png",
          "iconName" : "Chess.com",
          "link" : "https://www.chess.com/news/view/2021-speed-chess-championship-main-event-giri-duda",
-         "pubDate" : "Mon, 08 Nov 2021 17:41:14 -0800",
+         "pubDate" : "Mon, 08 Nov 2021 17:41:00 -0800",
          "title" : "Speed Chess Championship: Giri Defeats Duda, Advances To Quarterfinals"
       },
       {
@@ -1986,14 +2010,6 @@ news_feed (
          "title" : "Episode 248- GM Noel Studer "
       },
       {
-         "description" : "Rook endings often have drawish tendencies, but pawn races usually spice things up. In the diagram position it is Black to move. Should he protect his pawn on g6 or should he attack White's pawn on b3?",
-         "iconLink" : "https://cypresschess.github.io/web/chessbase2.png",
-         "iconName" : "ChessBase",
-         "link" : "https://en.chessbase.com/post/pawn-race-in-a-rook-ending",
-         "pubDate" : "Tue, 19 Oct 2021 13:00:00 +0100",
-         "title" : "Pawn race in a rook ending"
-      },
-      {
          "description" : "",
          "iconLink" : "https://cypresschess.github.io/web/fide.png",
          "iconName" : "FIDE",
@@ -2008,14 +2024,6 @@ news_feed (
          "link" : "https://worldchess.com/news/all/the-algorand-series-the-final/",
          "pubDate" : "Tue, 19 Oct 2021 09:15:35 +0000",
          "title" : "The Algorand Series: the Final"
-      },
-      {
-         "description" : " &#8220;A dream becomes a goal when action is taken toward its achievement&#8221; Bo Bennett (businessman) As a junior I knew this under the name &#8220;Russian defence&#8221;. For quite a while it did not make much sense to me why any player would use it. Of course at that time I wanted to get an edge <a href=\"https://chessimprover.com/need-sure-points-petroff-defence-edition/\" class=\"more-link\">Continue reading<span class=\"screen-reader-text\"> \"Need sure points? Petroff Defence Edition\"</span></a> The post <a rel=\"nofollow\" href=\"https://chessimprover.com/need-sure-points-petroff-defence-edition/\">Need sure points? Petroff Defence Edition</a> appeared first on <a rel=\"nofollow\" href=\"https",
-         "iconLink" : "https://cypresschess.github.io/web/improver.png",
-         "iconName" : "The Chess<br>Improver",
-         "link" : "https://chessimprover.com/need-sure-points-petroff-defence-edition/",
-         "pubDate" : "Tue, 19 Oct 2021 07:39:56 +0000",
-         "title" : "Need sure points? Petroff Defence Edition"
       }
    ]
 }
