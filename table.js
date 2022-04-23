47780cd56dda6d480ffcee330969538321fc5722
cypresschess <cypresschess@example.com>
2022-04-23 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,6 @@
 jtable([
+"SUGAR LAND","2022-04-22","6","<a href=http://www.uschess.org/msa/XtblMain.php?202204220342 target='_blank'>FBCC04222022</a>",
+"CYPRESS","2022-04-22","14","<a href=http://www.uschess.org/msa/XtblMain.php?202204220262 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "HOUSTON","2022-04-16","5","<a href=http://www.uschess.org/msa/XtblMain.php?202204167772 target='_blank'>QUICKSWISS</a>",
 "SPRING","2022-04-16","16","<a href=http://www.uschess.org/msa/XtblMain.php?202204167412 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "CYPRESS","2022-04-15","18","<a href=http://www.uschess.org/msa/XtblMain.php?202204157342 target='_blank'>KNIGHTS OF CYPRESS</a>",
@@ -197,6 +199,4 @@ jtable([
 "CYPRESS","2021-06-04","4","<a href=http://www.uschess.org/msa/XtblMain.php?202106044722 target='_blank'>JUNE 4 SECTION B</a>",
 "SPRING","2021-06-04","23","<a href=http://www.uschess.org/msa/XtblMain.php?202106043802 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2021-06-04","8","<a href=http://www.uschess.org/msa/XtblMain.php?202106043762 target='_blank'>FBCC 2021 JUNE 3</a>",
-"SUGAR LAND","2021-05-30","6","<a href=http://www.uschess.org/msa/XtblMain.php?202105302192 target='_blank'>FBCC MAY 2021 MONTH-OF-SUNDAYS</a>",
-"SPRING","2021-05-28","21","<a href=http://www.uschess.org/msa/XtblMain.php?202105281972 target='_blank'>THE FRIDAY KNIGHTS</a>",
 ]);
