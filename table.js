d5e0485c8a0ab6fb2cd88f4c014d526bbf6f4a9b
cypresschess <cypresschess@example.com>
2022-08-13 22:13:04 -0500

    Okay

--- table.js
+++ table.js
@@ -1,5 +1,7 @@
 jtable([
+"SPRING","2022-08-12","32","<a href=http://www.uschess.org/msa/XtblMain.php?202208123052 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2022-08-12","6","<a href=http://www.uschess.org/msa/XtblMain.php?202208122982 target='_blank'>FBCC08122022</a>",
+"CYPRESS","2022-08-12","26","<a href=http://www.uschess.org/msa/XtblMain.php?202208122922 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "MISSOURI CITY","2022-08-09","10","<a href=http://www.uschess.org/msa/XtblMain.php?202208092002 target='_blank'>RCC QUADS AUGUST 9 2022</a>",
 "HOUSTON","2022-08-05  - 08-07","35","<a href=http://www.uschess.org/msa/XtblMain.php?202208071212 target='_blank'>$3,500 HOUSTON CHESS STUDIO AUGUST</a>",
 "SPRING","2022-08-05","29","<a href=http://www.uschess.org/msa/XtblMain.php?202208059882 target='_blank'>THE FRIDAY KNIGHTS</a>",
@@ -197,6 +199,4 @@ jtable([
 "HOUSTON","2021-10-16  - 10-17","237","<a href=http://www.uschess.org/msa/XtblMain.php?202110172542 target='_blank'>TCR: HOUSTON FALL CHAMPIONSHIP 2021</a>",
 "CYPRESS","2021-10-15","20","<a href=http://www.uschess.org/msa/XtblMain.php?202110151752 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SPRING","2021-10-15","8","<a href=http://www.uschess.org/msa/XtblMain.php?202110151742 target='_blank'>THE FRIDAY KNIGHTS</a>",
-"HOUSTON","2021-10-10","12","<a href=http://www.uschess.org/msa/XtblMain.php?202110100632 target='_blank'>SUNDAY SWISS OCT 10TH</a>",
-"HOUSTON","2021-10-09","120","<a href=http://www.uschess.org/msa/XtblMain.php?202110099942 target='_blank'>NAVARRO MIDDLE SCHOOL SCHOLASTIC</a>",
 ]);
