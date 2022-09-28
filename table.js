217d8b2334f4672d87ad884abdeeefa20a384a71
cypresschess <cypresschess@example.com>
2022-09-28 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,6 @@
 jtable([
+"PEARLAND","2022-09-27","5","<a href=http://www.uschess.org/msa/XtblMain.php?202209276622 target='_blank'>RCC QUADS SEPTEMBER 27, 2022</a>",
+"LEAGUE CITY","2022-09-12  - 09-26","15","<a href=http://www.uschess.org/msa/XtblMain.php?202209262192 target='_blank'>CLCC SEPTEMBER MONDAYS</a>",
 "HOUSTON","2022-09-25","8","<a href=http://www.uschess.org/msa/XtblMain.php?202209255192 target='_blank'>SEPT 25TH SUNDAY SWISS</a>",
 "HOUSTON","2022-09-24","45","<a href=http://www.uschess.org/msa/XtblMain.php?202209244162 target='_blank'>SEPTEMBER SCHOLASTIC</a>",
 "CYPRESS","2022-09-23","38","<a href=http://www.uschess.org/msa/XtblMain.php?202209235702 target='_blank'>KNIGHTS OF CYPRESS</a>",
@@ -197,6 +199,4 @@ jtable([
 "HOUSTON","2022-01-02","10","<a href=http://www.uschess.org/msa/XtblMain.php?202201023642 target='_blank'>HOUSTON JAN. 2 SUNDAY SWISS</a>",
 "HOUSTON","2021-12-07  - 12-28","13","<a href=http://www.uschess.org/msa/XtblMain.php?202112282322 target='_blank'>DECEMBER TUESDAY NIGHT SWISS</a>",
 "HOUSTON","2021-12-26","7","<a href=http://www.uschess.org/msa/XtblMain.php?202112261752 target='_blank'>HOUSTON DEC. 26 SUNDAY SWISS</a>",
-"HOUSTON","2021-12-19","6","<a href=http://www.uschess.org/msa/XtblMain.php?202112199972 target='_blank'>HOUSTON DEC. 19 SUNDAY SWISS</a>",
-"SPRING","2021-12-17","22","<a href=http://www.uschess.org/msa/XtblMain.php?202112179402 target='_blank'>THE FRIDAY KNIGHTS</a>",
 ]);
