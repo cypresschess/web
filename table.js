3ca81d851c7ea53200996f60cbcf2eacc2493ee8
cypresschess <cypresschess@example.com>
2022-10-12 01:30:08 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"PEARLAND","2022-10-11","8","<a href=http://www.uschess.org/msa/XtblMain.php?202210113402 target='_blank'>RCC QUADS OCTOBER 11, 2022</a>",
 "HOUSTON","2022-10-08  - 10-09","98","<a href=http://www.uschess.org/msa/XtblMain.php?202210092182 target='_blank'>TCR: FALL CHAMPIONSHIP 2022</a>",
 "HOUSTON","2022-10-08","131","<a href=http://www.uschess.org/msa/XtblMain.php?202210081252 target='_blank'>TCR: FALL SCHOLASTIC ON 'NATIONAL CHESS DAY'</a>",
 "SUGAR LAND","2022-10-07","3","<a href=http://www.uschess.org/msa/XtblMain.php?202210070772 target='_blank'>FBCC 2022 LUCKY 7 OPEN</a>",
@@ -198,5 +199,4 @@ jtable([
 "CYPRESS","2022-01-21","16","<a href=http://www.uschess.org/msa/XtblMain.php?202201219862 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SPRING","2022-01-21","22","<a href=http://www.uschess.org/msa/XtblMain.php?202201210062 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "HOUSTON","2022-01-15  - 01-16","54","<a href=http://www.uschess.org/msa/XtblMain.php?202201169062 target='_blank'>$4,000 HOUSTON CHESS STUDIO OPEN</a>",
-"HOUSTON","2022-01-15","44","<a href=http://www.uschess.org/msa/XtblMain.php?202201157612 target='_blank'>WHARTON ACADEMY SCHOLASTIC</a>",
 ]);
