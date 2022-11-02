c1f488b40a21ccf397c197239fa04524b89dfed8
cypresschess <cypresschess@example.com>
2022-11-02 01:30:08 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"PEARLAND","2022-11-01","6","<a href=http://www.uschess.org/msa/XtblMain.php?202211015002 target='_blank'>RCC QUADS NOVEMBER 1, 2022</a>",
 "HOUSTON","2022-10-30","16","<a href=http://www.uschess.org/msa/XtblMain.php?202210303962 target='_blank'>OCTOBER  30 SUNDAY SWISS</a>",
 "HOUSTON","2022-10-29","26","<a href=http://www.uschess.org/msa/XtblMain.php?202210292932 target='_blank'>OCTOBER SCHOLASTIC</a>",
 "CYPRESS","2022-10-28","24","<a href=http://www.uschess.org/msa/XtblMain.php?202210282552 target='_blank'>KNIGHTS OF CYPRESS</a>",
@@ -198,5 +199,4 @@ jtable([
 "SPRING","2022-02-25","34","<a href=http://www.uschess.org/msa/XtblMain.php?202202254512 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "HOUSTON","2022-02-01  - 02-22","12","<a href=http://www.uschess.org/msa/XtblMain.php?202202223562 target='_blank'>FEBRUARY TUESDAY NIGHT SWISS</a>",
 "HOUSTON","2022-02-20","10","<a href=http://www.uschess.org/msa/XtblMain.php?202202203192 target='_blank'>HOUSTON FEB 20 SUNDAY SWISS</a>",
-"HOUSTON","2022-02-19","157","<a href=http://www.uschess.org/msa/XtblMain.php?202202191962 target='_blank'>BLANSON HS CHESS TOURNAMENT</a>",
 ]);
