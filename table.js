46c5bae2175c517bbd48aad6dbaff292134a7bad
cypresschess <cypresschess@example.com>
2022-11-20 01:30:08 -0600

    Okay

--- table.js
+++ table.js
@@ -1,5 +1,6 @@
 jtable([
 "CYPRESS","2022-11-18","30","<a href=http://www.uschess.org/msa/XtblMain.php?202211184262 target='_blank'>KNIGHTS OFCYPRESS</a>",
+"SPRING","2022-11-18","19","<a href=http://www.uschess.org/msa/XtblMain.php?202211184242 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2022-11-18","6","<a href=http://www.uschess.org/msa/XtblMain.php?202211184222 target='_blank'>FBCC 2022 MID-NOVEMBER OPEN</a>",
 "PEARLAND","2022-11-15","4","<a href=http://www.uschess.org/msa/XtblMain.php?202211152632 target='_blank'>RCC QUADS NOVEMBER 15 2022</a>",
 "HOUSTON","2022-11-14","2","<a href=http://www.uschess.org/msa/XtblMain.php?202211142562 target='_blank'>MATCH PLAY</a>",
@@ -198,5 +199,4 @@ jtable([
 "SPRING","2022-03-18","18","<a href=http://www.uschess.org/msa/XtblMain.php?202203184922 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2022-03-18","8","<a href=http://www.uschess.org/msa/XtblMain.php?202203184902 target='_blank'>TOURNAMENTS/FBCC 2022 ST. PATRICK O</a>",
 "CYPRESS","2022-03-18","12","<a href=http://www.uschess.org/msa/XtblMain.php?202203184852 target='_blank'>KNIGHTS OF CYPRESS</a>",
-"HOUSTON","2022-03-13","14","<a href=http://www.uschess.org/msa/XtblMain.php?202203132602 target='_blank'>MARCH 13 SUNDAY SWISS</a>",
 ]);
