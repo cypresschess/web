42db6ab351605fc6c4500c844af710ccac027d95
cypresschess <cypresschess@example.com>
2022-11-28 01:30:08 -0600

    Okay

--- table.js
+++ table.js
@@ -1,5 +1,6 @@
 jtable([
 "SPRING","2022-11-25","16","<a href=http://www.uschess.org/msa/XtblMain.php?202211257992 target='_blank'>THE FRIDAY KNIGHTS</a>",
+"CYPRESS","2022-11-25","19","<a href=http://www.uschess.org/msa/XtblMain.php?202211257972 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "PEARLAND","2022-11-22","12","<a href=http://www.uschess.org/msa/XtblMain.php?202211227182 target='_blank'>RCC QUADS NOVEMBER 22, 2022</a>",
 "HOUSTON","2022-11-19","21","<a href=http://www.uschess.org/msa/XtblMain.php?202211195212 target='_blank'>HOUSTON WOMEN AND GIRLS 2022 CHESS</a>",
 "CYPRESS","2022-11-18","30","<a href=http://www.uschess.org/msa/XtblMain.php?202211184262 target='_blank'>KNIGHTS OFCYPRESS</a>",
@@ -198,5 +199,4 @@ jtable([
 "SPRING","2022-03-25","21","<a href=http://www.uschess.org/msa/XtblMain.php?202203257862 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "CYPRESS","2022-03-25","22","<a href=http://www.uschess.org/msa/XtblMain.php?202203257802 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "HOUSTON","2022-03-01  - 03-22","13","<a href=http://www.uschess.org/msa/XtblMain.php?202203227022 target='_blank'>MARCH TUESDAY NIGHT SWISS</a>",
-"HOUSTON","2022-03-18  - 03-20","302","<a href=http://www.uschess.org/msa/XtblMain.php?202203206552 target='_blank'>2022 LONE STAR OPEN CHAMPIONSHIPS</a>",
 ]);
