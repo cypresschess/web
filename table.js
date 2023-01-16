4f025aef2d4df3d86246688aed36f426ea6eb9d8
cypresschess <cypresschess@example.com>
2023-01-16 01:30:08 -0600

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,6 @@
 jtable([
+"HOUSTON","2023-01-15","16","<a href=http://www.uschess.org/msa/XtblMain.php?202301153292 target='_blank'>JAN  15  SUNDAY SWISS</a>",
+"HOUSTON","2023-01-14","121","<a href=http://www.uschess.org/msa/XtblMain.php?202301142592 target='_blank'>WHARTON DUAL LANGUAGE ACADEMY TOURN</a>",
 "SPRING","2023-01-13","18","<a href=http://www.uschess.org/msa/XtblMain.php?202301131972 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "HOUSTON","2023-01-13","16","<a href=http://www.uschess.org/msa/XtblMain.php?202301131902 target='_blank'>REBECCA CHESS CLUB QUADS JAN 13, 2023</a>",
 "CYPRESS","2023-01-13","21","<a href=http://www.uschess.org/msa/XtblMain.php?202301131812 target='_blank'>KNIGHTS OF CYPRESS</a>",
@@ -197,6 +199,4 @@ jtable([
 "CYPRESS","2022-05-27","18","<a href=http://www.uschess.org/msa/XtblMain.php?202205277252 target='_blank'>KINGHTS OF CYPRESS</a>",
 "HOUSTON","2022-05-03  - 05-24","20","<a href=http://www.uschess.org/msa/XtblMain.php?202205246312 target='_blank'>MAY TUESDAY NIGHT SWISS</a>",
 "LEAGUE CITY","2022-05-02  - 05-23","11","<a href=http://www.uschess.org/msa/XtblMain.php?202205235742 target='_blank'>CLCC MARCH MONDAYS</a>",
-"HOUSTON","2022-05-21  - 05-22","41","<a href=http://www.uschess.org/msa/XtblMain.php?202205225042 target='_blank'>$1,500 HOUSTON CHESS STUDIO MAY CHA</a>",
-"SUGAR LAND","2022-05-20","6","<a href=http://www.uschess.org/msa/XtblMain.php?202205203542 target='_blank'>FBCC END OF SCHOOL OPEN 2022</a>",
 ]);
