45089b988b5da428c261914e811a7244b713cfef
cypresschess <cypresschess@example.com>
2023-04-09 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,6 @@
 jtable([
+"HOUSTON","2023-04-08","126","<a href=http://www.uschess.org/msa/XtblMain.php?202304081862 target='_blank'>TCR: EASTER TOURNAMENT 2023</a>",
+"CYPRESS","2023-04-08","18","<a href=http://www.uschess.org/msa/XtblMain.php?202304081612 target='_blank'>EVENT NAME MISSING</a>",
 "HOUSTON","2023-04-02","18","<a href=http://www.uschess.org/msa/XtblMain.php?202304029182 target='_blank'>APRIL 2ND 2023 SUNDAY SWISS</a>",
 "CYPRESS","2023-03-31","24","<a href=http://www.uschess.org/msa/XtblMain.php?202303318452 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SPRING","2023-03-31","27","<a href=http://www.uschess.org/msa/XtblMain.php?202303317942 target='_blank'>THE FRIDAY KNIGHTS</a>",
@@ -197,6 +199,4 @@ jtable([
 "SUGAR LAND","2022-08-19","6","<a href=http://www.uschess.org/msa/XtblMain.php?202208196782 target='_blank'>FBCC 2022 NATIONAL PHOTOGRAPHY OPEN</a>",
 "CYPRESS","2022-08-19","27","<a href=http://www.uschess.org/msa/XtblMain.php?202208196772 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "MISSOURI CITY","2022-08-16","6","<a href=http://www.uschess.org/msa/XtblMain.php?202208165542 target='_blank'>RCC QUADS AUGUST 16, 2022</a>",
-"LEAGUE CITY","2022-08-01  - 08-15","10","<a href=http://www.uschess.org/msa/XtblMain.php?202208154652 target='_blank'>CLCC AUGUST MONDAYS</a>",
-"HOUSTON","2022-08-14","12","<a href=http://www.uschess.org/msa/XtblMain.php?202208144392 target='_blank'>AUGUST 14 SUNDAY SWISS</a>",
 ]);
