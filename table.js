226fb87d73d40da51860e0e71665607eebb32d08
cypresschess <cypresschess@example.com>
2023-05-03 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"HOUSTON","2023-04-29","18","<a href=http://www.uschess.org/msa/XtblMain.php?202304295522 target='_blank'>SATURDAY ADULT QUADS - APRIL 29</a>",
 "HOUSTON","2023-04-29","556","<a href=http://www.uschess.org/msa/XtblMain.php?202304294212 target='_blank'>TH ROGERS SCHOLASTIC MAY 2023</a>",
 "CYPRESS","2023-04-28","31","<a href=http://www.uschess.org/msa/XtblMain.php?202304283222 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SPRING","2023-04-28","25","<a href=http://www.uschess.org/msa/XtblMain.php?202304283082 target='_blank'>THE FRIDAY KNIGHTS</a>",
@@ -198,5 +199,4 @@ jtable([
 "CYPRESS","2022-09-10","36","<a href=http://www.uschess.org/msa/XtblMain.php?202209107162 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SPRING","2022-09-09","22","<a href=http://www.uschess.org/msa/XtblMain.php?202209097092 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2022-09-09","12","<a href=http://www.uschess.org/msa/XtblMain.php?202209097062 target='_blank'>FBCC LABOR DAY CHAMPIONSHIP</a>",
-"HOUSTON","2022-09-04","6","<a href=http://www.uschess.org/msa/XtblMain.php?202209044942 target='_blank'>SEPT 4 SUNDAY SWISS</a>",
 ]);
