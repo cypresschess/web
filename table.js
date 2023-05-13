172e3bf0b4037ab7c87d87e5dac1f0ebc7fe6f3b
cypresschess <cypresschess@example.com>
2023-05-13 01:30:08 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,7 @@
 jtable([
+"SPRING","2023-05-12","39","<a href=http://www.uschess.org/msa/XtblMain.php?202305121052 target='_blank'>THE FRIDAY KNIGHTS</a>",
+"HOUSTON","2023-05-12","18","<a href=http://www.uschess.org/msa/XtblMain.php?202305121042 target='_blank'>REBECCA CHESS CLUB QUADS MAY 12, 2023</a>",
+"SUGAR LAND","2023-05-12","8","<a href=http://www.uschess.org/msa/XtblMain.php?202305121022 target='_blank'>FBCC 2023 MID-MAY OPEN/</a>",
 "HOUSTON","2023-05-07","24","<a href=http://www.uschess.org/msa/XtblMain.php?202305078802 target='_blank'>MAY 7TH 2023 SUNDAY SWISS</a>",
 "CYPRESS","2023-05-06","32","<a href=http://www.uschess.org/msa/XtblMain.php?202305067632 target='_blank'>EVENT NAME MISSING</a>",
 "HOUSTON","2023-05-05","22","<a href=http://www.uschess.org/msa/XtblMain.php?202305059232 target='_blank'>REBECCA CHESS CLUB QUADS MAY 5, 2023</a>",
@@ -196,7 +199,4 @@ jtable([
 "PEARLAND","2022-09-20","4","<a href=http://www.uschess.org/msa/XtblMain.php?202209202632 target='_blank'>RCC QUADS SEPTEMBER 20, 2022</a>",
 "HOUSTON","2022-09-17  - 09-18","52","<a href=http://www.uschess.org/msa/XtblMain.php?202209182832 target='_blank'>$3,000 HOUSTON CHESS STUDIO SEPTEMB</a>",
 "CYPRESS","2022-09-16","24","<a href=http://www.uschess.org/msa/XtblMain.php?202209160432 target='_blank'>KNIGHTS OF CYPRESS</a>",
-"SPRING","2022-09-16","23","<a href=http://www.uschess.org/msa/XtblMain.php?202209160202 target='_blank'>THE FRIDAY KNIGHTS</a>",
-"SUGAR LAND","2022-09-16","10","<a href=http://www.uschess.org/msa/XtblMain.php?202209160172 target='_blank'>FBCC 2022 END OF SUMMER OPEN</a>",
-"PEARLAND","2022-09-13","4","<a href=http://www.uschess.org/msa/XtblMain.php?202209139312 target='_blank'>RCC QUADS SEPTEMBER 13, 2022</a>",
 ]);
