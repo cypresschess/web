392fe35e9fd827444eb56223b19513e3ee99c3a0
cypresschess <cypresschess@example.com>
2023-09-04 22:30:32 -0500

    Okay

--- chess.js
+++ chess.js
@@ -2,12 +2,36 @@ news_feed (
 {
    "items" : [
       {
+         "description" : "In the rapid section of the Tata Steel India Tournament, Ju Wenjun was outscored by Divya Deshmukh. In blitz, however, the women's world champion was the strongest and won with 12&frac12;/ out of 18 points, finishing ahead of Humpy Koneru and Harika Dronavalli. | Photo: Vivek Sohani",
+         "iconLink" : "https://cypresschess.github.io/web/chessbase2.png",
+         "iconName" : "ChessBase",
+         "link" : "https://en.chessbase.com/post/tata-steel-india-2023-d5",
+         "pubDate" : "Tue, 05 Sep 2023 02:00:00",
+         "title" : "Ju Wenjun claims victory at Tata Steel India Blitz"
+      },
+      {
+         "description" : "<div class=\"body text-secondary\"> Grandmaster Benjamin Bok teaches a chess class about the preparation of top tournament chess players. 2023.08.31<br /> Anish Giri vs Dommaraju Gukesh, Tata Steel Masters (2023): D38 Queen&#039;s Gambit Declined, Ragozin variation<br /> <a href=\"https://www.chessgames.com/perl/chessgame?gid=2453794\">https://www.chessgames.com/perl/chessgame?gid=2453794</a> </div><div class=\"field field-name-field-author field-type-list-text field-label-hidden field-wrapper\">CCSCSL Staff</div><div class=\"field field-name-field-video-link field-type-video-embed-field field-label-hidden field-wrapper\"><a href=\"/blog/how-anish-giri-prepares-prepare-pro-gm-benjamin-bok\"><img src=\"",
+         "iconLink" : "https://cypresschess.github.io/web/stlouis2.png",
+         "iconName" : "Saint Louis<br>Chess Club",
+         "link" : "https://saintlouischessclub.org/blog/how-anish-giri-prepares-prepare-pro-gm-benjamin-bok",
+         "pubDate" : "Tue, 05 Sep 2023 00:29:45",
+         "title" : "How Anish Giri Prepares | Prepare Like A Pro - GM Benjamin Bok"
+      },
+      {
          "description" : "Dommaraju Gukesh is currently world number eight, with an Elo of 2758. The most amazing thing is that he has just turned seventeen years old! His strongest points are the attack and deep calculation. Vidit Gujrathi, who won the U14 World Championship in 2008, is currently rated 2716. His strengths are advanced tactics and calculations. Both these Indian Super-Grandmasters have produced Fritztrainers for ChessBase, which you can get for a special summer price this week: €64.90 instead of €79.80 for both packages.",
          "iconLink" : "https://cypresschess.github.io/web/chessbase2.png",
          "iconName" : "ChessBase",
          "link" : "https://en.chessbase.com/post/indian-summer-special",
          "pubDate" : "Mon, 04 Sep 2023 18:27:43",
          "title" : "Indian Summer Special"
+      },
+      {
+         "description" : " One week ago, Chess.com released a statement announcing the reinstatement of Hans Niemann on chess.com and an end to the legal dispute that gripped the chess world for the past year. The revelation raises some questions about how the matter unfolded and whether there was an out-of-court settlement. However, the terms of the agreement were The post <a rel=\"nofollow\" href=\"https://www.thechessdrum.net/blog/2023/09/04/aftermath-of-niemann-chess-com-carlsen-agreement/\">Aftermath of Niemann/Chess.com/Carlsen agreement</a> appeared first on <a rel=\"nofollow\" href=\"https://www.thechessdrum.net/blog\">The Chess Drum</a>. ",
+         "iconLink" : "https://cypresschess.github.io/web/drum3.png",
+         "iconName" : "The Chess<br>Drum",
+         "link" : "https://www.thechessdrum.net/blog/2023/09/04/aftermath-of-niemann-chess-com-carlsen-agreement/",
+         "pubDate" : "Mon, 04 Sep 2023 18:14:14",
+         "title" : "Aftermath of Niemann/Chess.com/Carlsen agreement"
       },
       {
          "description" : "The league stage of the Top Chess Engine Championship (Season 25) reaches the pinnacle of the event the Premier Division. The defending champion Stockfish will meet its main competitor Leela Chess Zero, as well as six more of the strongest chess engines: four seeded participants KomodoDragon, Ethereal, Stoofvlees, Slowchess and the two []",
@@ -24,6 +48,14 @@ news_feed (
          "link" : "https://timesofindia.indiatimes.com/sports/chess/ju-wenjun-pips-koneru-humpy-for-tata-steel-chess-blitz-title/articleshow/103367909.cms",
          "pubDate" : "Mon, 04 Sep 2023 17:34:18 +0000",
          "title" : "Ju Wenjun pips Koneru Humpy for Tata Steel Chess blitz title"
+      },
+      {
+         "description" : "&#34;He&#39;s finishing up his bingo card!&#34; said GM Eric Hansen as GM Hikaru Nakamura managed to play 14 of the 16 possible pawn pushes with the white pieces on move 1 on the way to beating GM Yu Yangyi 19-9 in the Round of 16 of the 2023 Chess.com Speed ...",
+         "iconLink" : "https://cypresschess.github.io/web/chesscom.png",
+         "iconName" : "Chess.com",
+         "link" : "https://www.chess.com/news/view/2023-speed-chess-championship-r16-nakamura-yu",
+         "pubDate" : "Mon, 04 Sep 2023 17:19:00",
+         "title" : "Nakamura Plays Opening Bingo To Beat Yu"
       },
       {
          "description" : "On 1st September FIDE published its new rating list. Magnus Carlsen is still the clear number one, although he only gained 4 rating points by winning the prestigious World Cup. Carlsen tops the list with a rating of 2839 points, 53 points ahead of Fabiano Caruana, who has replaced Hikaru Nakamura as world number 2.",
@@ -80,14 +112,6 @@ news_feed (
          "link" : "https://en.chessbase.com/post/generation-cup-2023-d5",
          "pubDate" : "Mon, 04 Sep 2023 08:10:00",
          "title" : "Carlsen beats Firouzja in tough match, wins Generation Cup"
-      },
-      {
-         "description" : "The Tata Steel Chess India tournaments are taking place from August 31 to September 9 at the Bhasa Bhawan Auditorium of the majestic National Library in Kolkata. The rapid section of the women's event is being played from Thursday to Saturday, with Ju Wenjun, Humpy Koneru and Harika Dronavalli as the top seeds. | Follow the games live with expert commentary starting at 10.30 CEST (4.30 ET, 14.00 IST)",
-         "iconLink" : "https://cypresschess.github.io/web/chessbase2.png",
-         "iconName" : "ChessBase",
-         "link" : "https://en.chessbase.com/post/tata-steel-india-2023-live",
-         "pubDate" : "Mon, 04 Sep 2023 08:05:00",
-         "title" : "Tata Steel Chess India - Live!"
       },
       {
          "description" : " It was with a feeling of modified rapture that I greeted the recent news of government funding for chess in England. Increased money for our national teams? Great, as long as it's used wisely. I'm not complaining about that. Chess boards in parks? A pointless gimmick designed to grab headlines which serves little purpose. All <a href=\"https://chessimprover.com/modified-rapture/\" class=\"more-link\">Continue reading<span class=\"screen-reader-text\"> \"Modified Rapture\"</span></a> The post <a rel=\"nofollow\" href=\"https://chessimprover.com/modified-rapture/\">Modified Rapture</a> appeared first on <a rel=\"nofollow\" href=\"https://chessimprover.com\">The Chess Improver</a>. ",
@@ -914,14 +938,6 @@ news_feed (
          "title" : "FIDE World Rapid Team Championship: WR Chess continues dominance with perfect streak"
       },
       {
-         "description" : "After two days, WR Chess soars ahead of the field at the World Rapid Team Championship. Led by perfect scores from GMs&#38;nbsp;Praggnanandhaa Rameshbabu and Hou Yifan, this star-studded team has won eight matches in a row to gain a three-point margin...",
-         "iconLink" : "https://cypresschess.github.io/web/chesscom.png",
-         "iconName" : "Chess.com",
-         "link" : "https://www.chess.com/news/view/2023-world-rapid-team-championship-day-2",
-         "pubDate" : "Sun, 27 Aug 2023 18:19:00",
-         "title" : "Perfect Scores By Praggnanandhaa, Hou Make WR Chess Unstoppable"
-      },
-      {
          "description" : "After so many years of posting on <A HREF=\"https://chessforallages.blogspot.com/2016/11/the-sociology-of-chess.html\">The Sociology of Chess</A> (November 2016), it finally occurred to me to ask the question, 'What is the difference between a sociologist and a historian?' Google pointed me to <A HREF=\"https://byjusexamprep.com/upsc-exam/difference-between-sociology-and-history\">Difference Between Sociology and History</A> (byjusexamprep.com), where I learned, <BLOCKQUOTE> The key difference between sociology and history is that sociology is a social science that focuses on the contemporary social environment. However, history deals with the past and examines previous societies. Sociology is a",
          "iconLink" : "https://cypresschess.github.io/web/blogger2.png",
          "iconName" : "Chess for<br>All Ages",
@@ -1696,22 +1712,6 @@ news_feed (
          "link" : "https://chessimprover.com/the-rambling-rook-2/",
          "pubDate" : "Tue, 15 Aug 2023 13:47:09",
          "title" : "The Rambling Rook (2)"
-      },
-      {
-         "description" : "Gukesh, Praggnanandhaa, Erigaisi and Vidit have made it 4 Indians out of the 8 men's quarterfinalists. Meanwhile, Harika lost her quarterfinal match on the women's side.",
-         "iconLink" : "https://cypresschess.github.io/web/ESPN.png",
-         "iconName" : "ESPN",
-         "link" : "https://www.espn.com/chess/story/_/id/38190699/record-india-magnus-carlsen-d-gukesh-chess-world-cup-headline-quarterfinals-praggnanandhaa-arjun-erigaisi-vidit-gujrathi?device=featurephone",
-         "pubDate" : "Tue, 15 Aug 2023 02:41:18",
-         "title" : "Magnus vs Gukesh headlines India's record-breaking Chess World Cup quarters"
-      },
-      {
-         "description" : "",
-         "iconLink" : "https://cypresschess.github.io/web/fide.png",
-         "iconName" : "FIDE",
-         "link" : "https://www.fide.com/news/2581",
-         "pubDate" : "Mon, 14 Aug 2023 23:47:36 +0000",
-         "title" : "FIDE World Youth Olympiad 2023: Day 2 Recap"
       }
    ]
 }
