404e0c45d2e694e8837dd58ea81fee8fdf0e16cf
cypresschess <cypresschess@example.com>
2024-10-30 01:30:06 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"HOUSTON","2024-10-01  - 10-29","22","<a href=http://www.uschess.org/msa/XtblMain.php?202410297902 target='_blank'>OCTOBER 2024 TUESDAY NIGHT SWISS</a>",
 "HOUSTON","2024-10-26  - 10-27","21","<a href=http://www.uschess.org/msa/XtblMain.php?202410276462 target='_blank'>HOUSTON SENIOR OPEN 2024</a>",
 "HOUSTON","2024-10-26","10","<a href=http://www.uschess.org/msa/XtblMain.php?202410265432 target='_blank'>CHESSPANDA10262024SATURDAY</a>",
 "CYPRESS","2024-10-25","26","<a href=http://www.uschess.org/msa/XtblMain.php?202410255362 target='_blank'>KNIGHTS OF CYPRESS</a>",
@@ -198,5 +199,4 @@ jtable([
 "SPRING","2024-03-22","33","<a href=http://www.uschess.org/msa/XtblMain.php?202403223012 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "CYPRESS","2024-03-22","25","<a href=http://www.uschess.org/msa/XtblMain.php?202403222902 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SUGAR LAND","2024-03-22","8","<a href=http://www.uschess.org/msa/XtblMain.php?202403222812 target='_blank'>FBCC 2024 NATIONAL PUPPY OPEN</a>",
-"HOUSTON","2024-03-22","8","<a href=http://www.uschess.org/msa/XtblMain.php?202403222772 target='_blank'>REBECCA CHESS CLUB QUADS MARCH 22,2024</a>",
 ]);
