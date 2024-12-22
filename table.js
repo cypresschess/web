af7327b6e6d88209b02f4875441c7d1e20ccd950
cypresschess <cypresschess@example.com>
2024-12-22 01:30:07 -0600

    Okay

--- table.js
+++ table.js
@@ -1,5 +1,8 @@
 jtable([
+"HOUSTON","2024-12-21","8","<a href=http://www.uschess.org/msa/XtblMain.php?202412219422 target='_blank'>CHESSPANDA12212024SATURDAY</a>",
+"HOUSTON","2024-12-21","31","<a href=http://www.uschess.org/msa/XtblMain.php?202412219192 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2024-12-20","8","<a href=http://www.uschess.org/msa/XtblMain.php?202412209112 target='_blank'>FBCC 2024 NATIONAL SANGRIA DAY OPEN</a>",
+"CYPRESS","2024-12-20","30","<a href=http://www.uschess.org/msa/XtblMain.php?202412209062 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "HOUSTON","2024-12-03  - 12-17","13","<a href=http://www.uschess.org/msa/XtblMain.php?202412178022 target='_blank'>DECEMBER 2024 TUESDAY NIGHT SWISS</a>",
 "HOUSTON","2024-12-14","63","<a href=http://www.uschess.org/msa/XtblMain.php?202412146592 target='_blank'>DEC 14TH - TROPHY SCHOLASTIC</a>",
 "HOUSTON","2024-12-14","10","<a href=http://www.uschess.org/msa/XtblMain.php?202412144872 target='_blank'>CHESSPANDA12142024SATURDAY</a>",
@@ -196,7 +199,4 @@ jtable([
 "HOUSTON","2024-05-04","8","<a href=http://www.uschess.org/msa/XtblMain.php?202405042372 target='_blank'>CHESSPANDA05042024SATURDAY</a>",
 "HOUSTON","2024-05-03","27","<a href=http://www.uschess.org/msa/XtblMain.php?202405031962 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2024-05-03","6","<a href=http://www.uschess.org/msa/XtblMain.php?202405031922 target='_blank'>FBCC 2024 NATIONAL SPACE DAY OPEN.P</a>",
-"CYPRESS","2024-05-03","20","<a href=http://www.uschess.org/msa/XtblMain.php?202405031772 target='_blank'>KNIGHTS OF CYPRESS</a>",
-"HOUSTON","2024-04-02  - 04-30","22","<a href=http://www.uschess.org/msa/XtblMain.php?202404300932 target='_blank'>APRIL 2024 TUESDAY NIGHT SWISS</a>",
-"HOUSTON","2024-04-28","17","<a href=http://www.uschess.org/msa/XtblMain.php?202404289942 target='_blank'>FREE THE US VIRGIN ISLANDS BENEFIT</a>",
 ]);
