84682ca748a3200a2302672912d205b3281b3ce8
cypresschess <cypresschess@example.com>
2025-07-13 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,7 @@
 jtable([
+"HOUSTON","2025-07-12","97","<a href=http://www.uschess.org/msa/XtblMain.php?202507122642 target='_blank'>2025 HOUSTON CHESS FESTIVAL SCHOLAS</a>",
+"HOUSTON","2025-07-12","5","<a href=http://www.uschess.org/msa/XtblMain.php?202507122222 target='_blank'>CHESSPANDA07122025SATURDAY</a>",
+"SPRING","2025-07-11","25","<a href=http://www.uschess.org/msa/XtblMain.php?202507111762 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2025-07-11","12","<a href=http://www.uschess.org/msa/XtblMain.php?202507111682 target='_blank'>FBCC 2025 DENNY'S SUMMERTIME OPEN</a>",
 "HOUSTON","2025-07-06","8","<a href=http://www.uschess.org/msa/XtblMain.php?202507069362 target='_blank'>BELLAIRE SUNDAY SWISS 07.06.2025</a>",
 "HOUSTON","2025-07-04  - 07-06","35","<a href=http://www.uschess.org/msa/XtblMain.php?202507069332 target='_blank'>$2,500 JULY CHAMPIONSHIP</a>",
@@ -196,7 +199,4 @@ jtable([
 "CYPRESS","2024-12-27","21","<a href=http://www.uschess.org/msa/XtblMain.php?202412272562 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "SPRING","2024-12-27","18","<a href=http://www.uschess.org/msa/XtblMain.php?202412272452 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "HOUSTON","2024-12-22","20","<a href=http://www.uschess.org/msa/XtblMain.php?202412220542 target='_blank'>SUNDAY SWISS AT NOON DEC. 22ND</a>",
-"HOUSTON","2024-12-21","8","<a href=http://www.uschess.org/msa/XtblMain.php?202412219422 target='_blank'>CHESSPANDA12212024SATURDAY</a>",
-"HOUSTON","2024-12-21","31","<a href=http://www.uschess.org/msa/XtblMain.php?202412219192 target='_blank'>THE FRIDAY KNIGHTS</a>",
-"SUGAR LAND","2024-12-20","8","<a href=http://www.uschess.org/msa/XtblMain.php?202412209112 target='_blank'>FBCC 2024 NATIONAL SANGRIA DAY OPEN</a>",
 ]);
