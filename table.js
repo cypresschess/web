ebd571d31da10c41f57348db39683fbdeae4279f
cypresschess <cypresschess@example.com>
2025-08-11 01:30:07 -0500

    Okay

--- table.js
+++ table.js
@@ -1,5 +1,8 @@
 jtable([
+"HOUSTON","2025-08-10","16","<a href=http://www.uschess.org/msa/XtblMain.php?202508100572 target='_blank'>BELLAIRE SUNDAY SWISS 08/10/25</a>",
+"HOUSTON","2025-08-10","4","<a href=http://www.uschess.org/msa/XtblMain.php?202508100382 target='_blank'>AUGUST 10TH-SUNDAY SWISS OPEN</a>",
 "HOUSTON","2025-08-09","38","<a href=http://www.uschess.org/msa/XtblMain.php?202508099292 target='_blank'>AUG 9TH -- TROPHY SCHOLASTIC</a>",
+"HOUSTON","2025-08-09","11","<a href=http://www.uschess.org/msa/XtblMain.php?202508090792 target='_blank'>CHESSPANDA08092025SATURDAY</a>",
 "SPRING","2025-08-08","22","<a href=http://www.uschess.org/msa/XtblMain.php?202508088882 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "SUGAR LAND","2025-08-08","20","<a href=http://www.uschess.org/msa/XtblMain.php?202508088802 target='_blank'>FBCC 2025 INTERNATIONAL CAT DAY OPE</a>",
 "CYPRESS","2025-08-08","27","<a href=http://www.uschess.org/msa/XtblMain.php?202508088742 target='_blank'>KNIGHTS OF CYPRESS</a>",
@@ -196,7 +199,4 @@ jtable([
 "SPRING","2025-01-31","29","<a href=http://www.uschess.org/msa/XtblMain.php?202501312572 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "CYPRESS","2025-01-31","25","<a href=http://www.uschess.org/msa/XtblMain.php?202501312492 target='_blank'>KNIGHTS OF CYPRESS</a>",
 "HOUSTON","2025-01-07  - 01-28","20","<a href=http://www.uschess.org/msa/XtblMain.php?202501281382 target='_blank'>JANUARY 2025 TUESDAY NIGHT SWISS</a>",
-"HOUSTON","2025-01-26","8","<a href=http://www.uschess.org/msa/XtblMain.php?202501269532 target='_blank'>JAN 26 SUNDAY SWISS AT NOON</a>",
-"SPRING","2025-01-25","374","<a href=http://www.uschess.org/msa/XtblMain.php?202501259962 target='_blank'>25TH KLEIN WINTER SCHOLASTIC</a>",
-"HOUSTON","2025-01-25","8","<a href=http://www.uschess.org/msa/XtblMain.php?202501258332 target='_blank'>QUADS - ALL AGES -SATURDAY JAN 25 A</a>",
 ]);
