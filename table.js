f3ee9d03f97f436391c7efe36f6f16ecae0724e9
cypresschess <cypresschess@example.com>
2025-12-12 01:30:06 -0600

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,5 @@
 jtable([
+"HOUSTON","2025-12-10","2","<a href=https://ratings.uschess.org/event/202512100183 target='_blank'>AW v JMcD</a>",
 "HOUSTON","2025-12-07","13","<a href=https://ratings.uschess.org/event/202512070363 target='_blank'>Bellaire Sunday Swiss 2025.12.07</a>",
 "HOUSTON","2025-12-07","30","<a href=https://ratings.uschess.org/event/202512070243 target='_blank'>$1,500 DECEMBER CHAMPIONSHIP</a>",
 "HOUSTON","2025-12-06","15","<a href=https://ratings.uschess.org/event/202512060393 target='_blank'>chesspanda12062025saturday</a>",
@@ -98,5 +99,4 @@ jtable([
 "HOUSTON","2025-09-05","18","<a href=https://ratings.uschess.org/event/202509054872 target='_blank'>THE FRIDAY KNIGHTS</a>",
 "HOUSTON","2025-09-01","23","<a href=https://ratings.uschess.org/event/202509013092 target='_blank'>2025 LABOR DAY BY AZCHACAD</a>",
 "HOUSTON","2025-08-31","14","<a href=https://ratings.uschess.org/event/202508311782 target='_blank'>BELLAIRE SUNDAY SWISS 8/31/2025</a>",
-"HOUSTON","2025-08-30","173","<a href=https://ratings.uschess.org/event/202508301742 target='_blank'>BACK TO SCHOOL SCHOLASTIC TOURNAMEN</a>",
 ]);
