af54c9bf29014e58686754fec209732a853f105b
cypresschess <cypresschess@example.com>
2025-12-18 01:30:06 -0600

    Okay

--- table.js
+++ table.js
@@ -6,6 +6,7 @@ jtable([
 "HOUSTON","2025-12-10","2","<a href=https://ratings.uschess.org/event/202512100183 target='_blank'>AW v JMcD</a>",
 "HOUSTON","2025-12-07","13","<a href=https://ratings.uschess.org/event/202512070363 target='_blank'>Bellaire Sunday Swiss 2025.12.07</a>",
 "HOUSTON","2025-12-07","30","<a href=https://ratings.uschess.org/event/202512070243 target='_blank'>$1,500 DECEMBER CHAMPIONSHIP</a>",
+"SUGAR LAND","2025-12-07","4","<a href=https://ratings.uschess.org/event/202512070103 target='_blank'>winter12072025</a>",
 "HOUSTON","2025-12-06","15","<a href=https://ratings.uschess.org/event/202512060393 target='_blank'>chesspanda12062025saturday</a>",
 "SUGAR LAND","2025-12-05","16","<a href=https://ratings.uschess.org/event/202512050403 target='_blank'>FBCC Levy Rozman Birthday Open</a>",
 "SPRING","2025-12-05","25","<a href=https://ratings.uschess.org/event/202512050333 target='_blank'>THE FRIDAY KNIGHTS</a>",
@@ -98,5 +99,4 @@ jtable([
 "HOUSTON","2025-09-07","33","<a href=https://ratings.uschess.org/event/202509076262 target='_blank'>$2,500 SEPTEMBER CHAMPIONSHIP</a>",
 "HOUSTON","2025-09-06","156","<a href=https://ratings.uschess.org/event/202509065642 target='_blank'>NOTTINGHAM ELEMENTARY SCHOLASTIC</a>",
 "HOUSTON","2025-09-06","14","<a href=https://ratings.uschess.org/event/202509065242 target='_blank'>CHESSPANDA09062025</a>",
-"CYPRESS","2025-09-05","38","<a href=https://ratings.uschess.org/event/202509054922 target='_blank'>KNIGHTS OF CYPRESS</a>",
 ]);
