a76c1bdec50fc1d9eeab3ce8e4e21d96eb32dd70
cypresschess <cypresschess@example.com>
2026-04-26 01:30:05 -0500

    Okay

--- table.js
+++ table.js
@@ -1,4 +1,6 @@
 jtable([
+"HOUSTON","2026-04-25","42","<a href=https://ratings.uschess.org/event/202604250663 target='_blank'>2026 TEXAS QUICK CHESS STATE CHAMPI</a>",
+"HOUSTON","2026-04-25","11","<a href=https://ratings.uschess.org/event/202604250383 target='_blank'>chesspanda04252026saturday</a>",
 "CYPRESS","2026-04-24","45","<a href=https://ratings.uschess.org/event/202604240183 target='_blank'>Knights of Cypress</a>",
 "SPRING","2026-04-24","11","<a href=https://ratings.uschess.org/event/202604240173 target='_blank'>THE FRIDAY KNIGHTS (TX)</a>",
 "SUGAR LAND","2026-04-24","8","<a href=https://ratings.uschess.org/event/202604240113 target='_blank'>FBCC 2026 Rainy Days Open</a>",
@@ -97,6 +99,4 @@ jtable([
 "CYPRESS","2026-02-06","37","<a href=https://ratings.uschess.org/event/202602060113 target='_blank'>Knights of Cypress</a>",
 "HOUSTON","2026-02-01","11","<a href=https://ratings.uschess.org/event/202602010273 target='_blank'>Bellaire Sunday Swiss 2026.02.01</a>",
 "HOUSTON","2026-02-01","6","<a href=https://ratings.uschess.org/event/202602010193 target='_blank'>2026 Houston Circuit T1 by AzChAcad</a>",
-"SPRING","2026-01-31","228","<a href=https://ratings.uschess.org/event/202601311013 target='_blank'>KLEIN WINTER SCHOLASTIC</a>",
-"HOUSTON","2026-01-31","10","<a href=https://ratings.uschess.org/event/202601310423 target='_blank'>chesspanda01312026saturday</a>",
 ]);
